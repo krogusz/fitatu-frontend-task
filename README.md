8bc63c68ddf11f43edc9e16c4688da923f39a8ab
Karolina <karolinarogusz@onet.pl>
2019-07-13 21:17:00 +0200

    tesk4: hamburger menu and updating README
    task4: hamburger menu
    
    updating readme
    
    README.md update

--- README.md
+++ README.md
@@ -4,17 +4,25 @@ Wybierz 3 dowolne zadania i je zrealizuj.
 
 ### Uruchomienie projektu:
 1. npm install
-1. npm run web
+2. npm run web
 
 ### Zadania:
 1. Dodaj komponent przycisku edycji każdego elementu listy i dodaj jako kolejną kolumnę do listy.
-1. Dodaj edycję elementu listy z zapisem do API (edycja tylko danych, które są na liście). Edycja może być wykonywana od razu na wierszu listy lub na nowym widoku z formularzem.
-1. Dostosuj widoki pod RWD.
-1. Dodaj menu (możesz wymyślić dodatkowe elementy, które nigdzie nie prowadzą), które na telefonach zamienia się w hamburger menu.
-1. Skonfiguruj środowisko do testów jednostkowych i napisz testy do widoku listy.
+2. Dodaj edycję elementu listy z zapisem do API (edycja tylko danych, które są na liście). Edycja może być wykonywana od razu na wierszu listy lub na nowym widoku z formularzem.
+3. Dostosuj widoki pod RWD.
+4. Dodaj menu (możesz wymyślić dodatkowe elementy, które nigdzie nie prowadzą), które na telefonach zamienia się w hamburger menu.
+5. Skonfiguruj środowisko do testów jednostkowych i napisz testy do widoku listy.
 
 ### Uwagi:
 1. Pobierz kopię tego repozytorium (nie rób forka), dodaj na swoje konto i ustaw publiczny dostęp.
-1. Każdy task wypushuj do mastera swojego repozytorium ze squashem i odpowiednim opisem w języku angielskim.
-1. Testowe API: https://github.com/typicode/jsonplaceholder#how-to
+2. Każdy task wypushuj do mastera swojego repozytorium ze squashem i odpowiednim opisem w języku angielskim.
+3. Testowe API: https://github.com/typicode/jsonplaceholder#how-to
+
+
+My tasks solution:
+1. task #1: I created this.edit element which says which row is edited. At first this.edit has value -1, but is changing when edit button is clicked (because of method edit_row). I used v-if and v-else constructions to determine (according to this.edit) what is displayed: data or editing inputs. 
+
+2. task #2: Every editing input received name and @input attributes. I also created new object - this.editedElem, which stores changing input's values. During editing user has two buttons available: save and reject. Save button runs save_row method, which gets as input an index of edited row and updates (fetch) the data in API, after that this.edit and this.editedElem are cleaned. Reject button sets up this.edit on -1 (no row is edited).
+
+3. task #4: I created &lt nav &gt tag (which contains menu list) and &l tspan &gt tag (which contains hamburger menu). I created also a new object this.classObject which is responsible for toggling class in \&lt nav &gt tag. Navbar is visible on bigger screen and when hamburger menu is clicked on smaller one (because of onMenuClick method). Hamburger menu is visible only on smaller screen. To get responsivity I used media queries.
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,19 @@
             <div class="page-header__logo" @click="onLogoClick">
                 <Logo />
             </div>
+            <span class="toogle">
+                <a href="#" @click="onMenuClick">☰</a>
+            </span>
+            <nav v-bind:class= "classObject" >
+                <div id="menu">
+                    <ul>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">contact</a></li>
+                        <li><a href="#">our history</a></li>
+                        <li><a href="#">our team</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     </header>
 </template>
@@ -11,6 +24,21 @@
     import Logo from '@img/fitatu.svg';
 
     export default {
+        data() {
+            return{
+                classObject: {
+                    'visible': false,
+                    'hidden': true,
+                }                
+            }
+        },
+
+        methods: {
+            onMenuClick(){
+                        this.classObject.visible = !this.classObject.visible;
+                        this.classObject.hidden = !this.classObject.hidden
+            }
+        },
         props: {
             onLogoClick: {
                 type: Function,
@@ -23,8 +51,12 @@
     };
 </script>
 <style lang="scss" scoped>
+
+    $break-small: 960px;
+
     .page-header {
         height: 60px;
+        
 
         &__content {
             display: flex;
@@ -37,6 +69,62 @@
             width: 100%;
             background-color: #fff;
             box-shadow: 0 0 15px #ced0d0;
+
+            nav{
+                position: absolute;
+                right: 2%;
+                li{
+                    display: inline-block;
+                    font-weight: 800;
+                    padding: 0 0 0 2rem;
+                    a{
+                        text-decoration:none;
+                        color: black;
+                    }
+                }
+            }
+
+            .toogle{
+                position: absolute;
+                right: 2%;
+                a{
+                    text-decoration: none;
+                    color: black;
+                    font-weight: 800;
+                    font-size: 1.3em;
+                }  
+            }
+
+            @media(min-width: $break-small){
+                .toogle{
+                    display: none;
+                }
+            }
+
+            @media(max-width: $break-small){
+                .hidden{
+                    display: none;
+                }
+                .visible{
+                    top: 2.83rem;
+                    right:0;
+                    width:100%;
+                    z-index:999;
+
+                    ul{
+                        padding:0;
+                        li{
+                            display: block;
+                            border-bottom: 1px solid #ced0d0;
+                            background:white;
+                            font-size: 1.3em;
+                            text-align:center;
+                            height: 45px;
+                            padding: 10px 0 0 0;
+                        }
+                    }
+                }                
+            }
         }
 
         &__logo {
